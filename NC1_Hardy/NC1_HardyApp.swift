48f098ed97d36a8b5bf4ffe26e91ec1124cd9882
Hardy <dusgh15378@gmail.com>
2022-05-03 09:12:15 +0900

    Initial Commit

---
+++ NC1_Hardy/NC1_HardyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_HardyApp.swift
+//  NC1_Hardy
+//
+//  Created by 김연호 on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct NC1_HardyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
